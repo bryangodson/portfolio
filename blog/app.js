fd2b5f5166efcca83d418e69dafa51860d87881b
bryangodson <ppdnquh@gmail.com>
2023-12-17 19:11:34 +0000

    with blog content

--- blog/app.js
+++ blog/app.js
@@ -4,3 +4,7 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("active");
   //   navigation_for_mobile.classList.toggle("active");
 });
+let scrollElements = document.querySelectorAll(".scrollreveal");
+scrollElements.forEach((element) => {
+  ScrollReveal({ reset: true, delay: 400 }).reveal(element);
+});
